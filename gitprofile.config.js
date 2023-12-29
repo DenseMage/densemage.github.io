dc6089cd867a4c59c9ed15e698e81b6c3582f55a
Shifan Hirani <21170438+hirashif@users.noreply.github.com>
2023-12-28 20:43:54 -0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,10 +4,10 @@ const config = {
   github: {
     username: 'hirashif', // Your GitHub org/user name. (Required)
     sortBy: 'updated', // stars | updated
-    limit: 5, // How many projects to display.
+    limit: 2, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['PrizePicksWebScraper', 'first-contributions', 'Time-Calculator'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
